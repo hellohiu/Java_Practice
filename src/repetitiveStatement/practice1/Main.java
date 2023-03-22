da92fedec606279a295abd0b679da0000e9af201
helloyuna <124885961+hellohiu@users.noreply.github.com>
2023-03-22 22:09:48 +0900

    백준 문제풀이 commit
    반복문

---
+++ src/repetitiveStatement/practice1/Main.java
@@ -0,0 +1,14 @@
+package src.repetitiveStatement.practice1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=0; i<9; i++){
+            System.out.println(n + " * " + (i+1) + " = " + n*(i+1));
+        }
+    }
+}
---
+++ src/repetitiveStatement/practice4/Main.java
@@ -0,0 +1,24 @@
+package src.repetitiveStatement.practice4;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int price = sc.nextInt();
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for(int i=0; i<n; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            sum += a*b;
+        }
+
+        if(price == sum){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
