e34d325e672f35be6154e4ed9f025c9c26d22202
helloyuna <124885961+hellohiu@users.noreply.github.com>
2023-05-14 00:36:52 +0900

    백준 문제풀이 commit

--- src/array2D/practice3/Main.java
+++ src/array2D/practice3/Main.java
@@ -5,7 +5,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[][] arr = new String[][];
-        
+        char[][] arr = new char[5][15];
+
+        for(int i=0; i<5; i++){
+            String str = sc.next();
+            for(int j=0; j<15; j++){
+                if(j<str.length()){
+                    arr[i][j] = str.charAt(j);
+                } else {
+                    arr[i][j] = ' ';
+                }
+            }
+        }
+
+        for(int i=0; i<15; i++){
+            for(int j=0; j<5; j++){
+                if(arr[j][i] == ' '){
+                    continue;
+                } else {
+                    System.out.print(arr[j][i]);
+                }
+            }
+        }
     }
 }
